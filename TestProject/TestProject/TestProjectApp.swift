861d5857547ccc3dd60fba70232c951077f56843
Krystsina Suvorava <>
2023-03-10 10:37:56 +0200

    Project was created

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Krystsina on 2023-03-10.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
